4ba24ed3dc63f2bebef534f1f4229efc163d19e7
dilshanperera98 <mldperera1998@gmail.com>
2025-02-25 14:48:07 +0530

    CV update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
              initial={{y:-30 , opacity:0}}
              whileInView={{y:0,opacity:1}}
              transition={{duration:0.8, delay:0.5}}
-            className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo '>Hi! I'm Dilshan Perera<Image src={assets.hand_icon} alt='' className='w-6' /> </motion.h3>
+            className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo '>Hi! I'm Dilshan <Image src={assets.hand_icon} alt='' className='w-6' /> </motion.h3>
             <motion.h1
             initial={{y:-20 , opacity:0}}
             whileInView={{y:0,opacity:1}}
@@ -46,7 +46,7 @@ const Header = () => {
                   initial={{y:30 , opacity:0}}
                   whileInView={{y:0,opacity:1}}
                   transition={{duration:0.6, delay:1.2}}
-                href={'Dilshan Perera - CV.pdf'} download className= 'bg-white dark:text-black px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 hover:bg-black hover:text-white dark:hover:bg-gray-800 dark:hover:text-white dark:border-white/50 transition-colors duration-500'>
+                href={'Dilshan Perera-CV.pdf'} download className= 'bg-white dark:text-black px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 hover:bg-black hover:text-white dark:hover:bg-gray-800 dark:hover:text-white dark:border-white/50 transition-colors duration-500'>
                 CV <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
               </svg>
