5f127e56ead3abb182167e963460b8e0374391cc
Dilshan Perera <83137780+dilshanperera98@users.noreply.github.com>
2025-04-01 13:40:46 +0530

    Comment out motion.a component in Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
                  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                </svg>
                </motion.a>
-                <motion.a
+                {/* <motion.a
                   initial={{y:30 , opacity:0}}
                   whileInView={{y:0,opacity:1}}
                   transition={{duration:0.6, delay:1.2}}
@@ -50,7 +50,7 @@ const Header = () => {
                 CV <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
               </svg>
-               </motion.a>
+               </motion.a> */}
             </div>
         </div>
     )
